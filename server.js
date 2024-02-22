47f9a2b6d335296e3e57e8e5edb3f3ca7b44f0fd
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2024-02-22 02:11:33 +0100

    add new templates for email response functionality

--- server.js
+++ server.js
@@ -2,30 +2,53 @@ const hbs = require("nodemailer-express-handlebars");
 const nodemailer = require("nodemailer");
 const path = require("path");
 const express = require("express");
+const handlebars = require("express-handlebars");
+const dotenv = require("dotenv");
+dotenv.config();
 
+const app = express();
+console.log(process.env.APP_EMAIL_ADDRESS);
+console.log(process.env.APP_EMAIL_PASSWORD);
+// app.set("view engine", "handlebars");
+
+// app.engine(
+//   "handlebars",
+//   handlebars.engine({
+//     layoutsDir: __dirname + "/views",
+//   })
+// );
+
+// app.set("views", path.join(__dirname, "views"));
 // // initialize nodemailer
-// var transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: "your_email@gmail.com",
-//     pass: "password_for_your_email_address",
-//   },
-// });
+var transporter = nodemailer.createTransport({
+  host: "mail.kelani.ng",
+  // secure: true,
+  port: 465,
+  auth: {
+    // user: "richardosunmu@gmail.com",
+    // pass: "rwtabhpraywtznck",
+    user: process.env.APP_EMAIL_ADDRESS,
+    pass: process.env.APP_EMAIL_PASSWORD,
+  },
+});
 
-// // point to the template folder
-// const handlebarOptions = {
-//   viewEngine: {
-//     partialsDir: path.resolve("./views/"),
-//     defaultLayout: false,
-//   },
-//   viewPath: path.resolve("./views/"),
-// };
+// point to the template folder
+const handlebarOptions = {
+  viewEngine: {
+    extname: ".handlebars",
+    partialsDir: path.resolve("./views/"),
+    layoutsDir: path.resolve("./views/"),
+    defaultLayout: false,
+  },
+  viewPath: path.resolve("./views/"),
+  extName: ".handlebars",
+};
 
-// // use a template file with nodemailer
-// transporter.use("compile", hbs(handlebarOptions));
+// use a template file with nodemailer
+transporter.use("compile", hbs(handlebarOptions));
 
 // const mailOptions = {
-//   from: '"My Company" <my@company.com>', // sender address
+//   from: 'Kelani', // sender address
 //   template: "email", // the name of the template file, i.e., email.handlebars
 //   to: "ronaldosunmu@gmail.com",
 //   subject: `Welcome to My Company, Ronald`,
@@ -40,13 +63,162 @@ const express = require("express");
 //   console.log(`Nodemailer error sending email to ${user.email}`, error);
 // }
 
-const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.post("/kelani-contact-us", (req, res) => {
-  const { name, email, phone, message } = req.body;
-  console.log(req.body);
-  return res.status(200).json({ message: "success" });
+app.post("/kelani-contact-us", async (req, res) => {
+  try {
+    const name = req.body.data.rows[0]["Name"];
+    const user = {
+      name,
+      email: req.body.data.rows[0]["Email"],
+    };
+
+    const mailOptions = {
+      from: process.env.APP_EMAIL_ADDRESS,
+      to: user.email,
+      subject: "We got your message",
+      template: "kelani-contact-us", //Use the rendered HTML from the template
+      context: {
+        name,
+      },
+    };
+
+    // Send email using Nodemailer
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email:", error);
+        res.status(500).send("Error sending email");
+      } else {
+        console.log("Email sent:", info.response);
+        res.status(200).send("Email sent successfully");
+      }
+    });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/kelani-power-contact-us", async (req, res) => {
+  try {
+    const name = req.body.data.rows[0]["Name"];
+    const user = {
+      name,
+      email: req.body.data.rows[0]["Email"],
+    };
+
+    const mailOptions = {
+      from: process.env.APP_EMAIL_ADDRESS,
+      to: user.email,
+      subject: "We got your message",
+      template: "kelani-power-contact-us", //Use the rendered HTML from the template
+      context: {
+        name,
+      },
+    };
+
+    // Send email using Nodemailer
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email:", error);
+        res.status(500).send("Error sending email");
+      } else {
+        console.log("Email sent:", info.response);
+        res.status(200).send("Email sent successfully");
+      }
+    });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/kelani-engineering-contact-us", async (req, res) => {
+  try {
+    const name = req.body.data.rows[0]["Name"];
+    const user = {
+      name,
+      email: req.body.data.rows[0]["Email"],
+    };
+
+    const mailOptions = {
+      from: process.env.APP_EMAIL_ADDRESS,
+      to: user.email,
+      subject: "We got your message",
+      template: "kelani-power-contact-us", //Use the rendered HTML from the template
+      context: {
+        name,
+      },
+    };
+
+    // Send email using Nodemailer
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email:", error);
+        res.status(500).send("Error sending email");
+      } else {
+        console.log("Email sent:", info.response);
+        res.status(200).send("Email sent successfully");
+      }
+    });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/kelani-consulting-contact-us", async (req, res) => {
+  try {
+    const name = req.body.data.rows[0]["Name"];
+    const user = {
+      name,
+      email: req.body.data.rows[0]["Email"],
+    };
+
+    const mailOptions = {
+      from: process.env.APP_EMAIL_ADDRESS,
+      to: user.email,
+      subject: "We got your message",
+      template: "kelani-power-contact-us", //Use the rendered HTML from the template
+      context: {
+        name,
+      },
+    };
+
+    // Send email using Nodemailer
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email:", error);
+        res.status(500).send("Error sending email");
+      } else {
+        console.log("Email sent:", info.response);
+        res.status(200).send("Email sent successfully");
+      }
+    });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/kelani-power-cta", async (req, res) => {
+  try {
+    const user = {
+      name: "Ronald Dosunmu",
+      email: "ronaldosunmu@gmail.com",
+    };
+
+    const mailOptions = {
+      from: "richardosunmu@gmail.com",
+      to: user.email,
+      subject: "Welcome to Our Platform",
+      template: "contact-us", //Use the rendered HTML from the template
+    };
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
